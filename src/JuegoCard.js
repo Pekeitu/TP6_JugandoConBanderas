ff9dec526e3260fd4283f70d2250afa8f8bd05ba
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-06-15 09:54:01 -0300

    FIXES

--- src/JuegoCard.js
+++ src/JuegoCard.js
@@ -20,27 +20,26 @@ function JuegoCard({ paises, paisCorrecto, puntos, setPuntos, seconds }){
     }
 
     return (
-    <Card className='FlexCentered'>
-      <h1>{puntos}</h1>
-    <Card.Img variant="top" className="flag" src={paises[paisCorrecto].flag}/>
-    <Card.Body>
-      <Card.Text>
-        <div className="d-grid gap-2">
-            {paises[paisCorrecto].name}
+      <Card className='FlexCentered'>
+        <h1>{puntos}</h1>
+        <Card.Img variant="top" className="flag" src={paises[paisCorrecto].flag}/>
+        <Card.Body>
+          <Card.Text>
+            <div className="d-grid gap-2">
+              {paises[paisCorrecto].name}
+            </div>
+          </Card.Text>
 
-              </div>
-            </Card.Text>
+          <Form onSubmit={handleEnvio}>
+            <Form.Group controlId='form.inputPais'>
+              <Form.Label> Ingrese el pais correcto </Form.Label>
+              <Form.Control type="text" name="inputPais" placeholder='Aqui aqui me cague'/>
+              <Button className='btn btn-primary' type='submit'>Enviar</Button>
+            </Form.Group>
+          </Form>
 
-            <Form onSubmit={handleEnvio}>
-              <Form.Group controlId='form.inputPais'>
-                <Form.Label> Ingrese el pais correcto </Form.Label>
-                <Form.Control type="text" name="inputPais" placeholder='Aqui aqui me cague'/>
-                <Button className='btn btn-primary' type='submit'>Enviar</Button>
-              </Form.Group>
-            </Form>
-          </Card.Body>
-        </Card>
-      </div>
+        </Card.Body>
+      </Card>
     );
 }
 
